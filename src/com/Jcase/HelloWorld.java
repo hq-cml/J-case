4dc4205fc079feb6adc7afb72379c21fb4e6354e
huaqi <huaqi@didichuxing.com>
2018-02-12 17:00:30 +0800

    HelloWorld

---
+++ src/com/Jcase/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.Jcase;
+
+/**
+ * Created by didi on 18/2/12.
+ */
+public class HelloWorld {
+    public static void main(String []args) {
+        System.out.println("Hello world");
+    }
+}
