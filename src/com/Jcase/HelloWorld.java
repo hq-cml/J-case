32ae65776eb747b15624eaefe0979c8a4db152c2
huaqi <huaqi@didichuxing.com>
2018-02-13 11:10:53 +0800

    scaner

--- src/com/Jcase/HelloWorld.java
+++ src/com/Jcase/HelloWorld.java
@@ -4,7 +4,7 @@ package com.Jcase;
  * Created by didi on 18/2/12.
  */
 public class HelloWorld {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
---
+++ src/com/Jcase/Scaner.java
@@ -0,0 +1,27 @@
+package com.Jcase;
+
+import java.util.Scanner;
+
+/**
+ * 类比C的scanf
+ */
+public class Scaner {
+    public static void main(String[] args) {
+        int[] iArray = new int[5];
+        int len = iArray.length;
+        int total = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入" + len +"个整数, 以空格分割:");
+
+        for (int i=0; i<len; i++) {
+            iArray[i] = sc.nextInt();
+        }
+
+        for (int i=0; i<len; i++) {
+            total += iArray[i];
+        }
+
+        System.out.println("所有元素和是:" + total);
+    }
+}
