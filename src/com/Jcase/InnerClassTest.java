adc83cd6dff7b271c5d8f8685b82dbcb5a911047
huaqi <huaqi@didichuxing.com>
2018-02-17 12:10:51 +0800

    内部类

---
+++ src/com/Jcase/InnerClassTest.java
@@ -0,0 +1,30 @@
+package com.Jcase;
+
+/**
+ * 内部类的使用
+ */
+
+public class InnerClassTest {
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+
+        inner.doStuff();
+        System.out.println(outer.size);
+        System.out.println(inner.counter);
+    }
+}
+
+//内部类的定义,必须先有外部类
+class Outer {
+    public int size;
+
+    //内部类
+    public class Inner {
+        public int counter = 10;
+        public void doStuff() {
+            size ++;
+        }
+    }
+}
