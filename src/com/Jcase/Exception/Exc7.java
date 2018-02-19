1a696a2178819ee42292c9b88937b6999ee7f448
huaqi <huaqi@didichuxing.com>
2018-02-19 11:02:23 +0800

    自定义异常类

---
+++ src/com/Jcase/Exception/Exc7.java
@@ -0,0 +1,34 @@
+package com.Jcase.Exception;
+
+/**
+ * 自定义异常类
+ */
+public class Exc7{
+
+    static void compute(int a) throws MyException {
+        System.out.println("Called compute(" + a + ")");
+        if(a > 10)
+            throw new MyException(a);
+        System.out.println("Normal exit");
+    }
+
+    public static void main(String args[]) {
+        try {
+            compute(1);
+            compute(20);
+        } catch (MyException e) {
+            System.out.println("Caught " + e);
+        }
+    }
+}
+
+// This program creates a custom exception type.
+class MyException extends Exception {
+    private int detail;
+    MyException(int a) {
+        detail = a;
+    }
+    public String toString() {
+        return "MyException[" + detail + "]";
+    }
+}
