752dce91d78861c48d7d42f54cde2384cc28b4ec
hq <hq_cml@163.com>
2018-04-06 19:56:20 +0800

    泛型类

---
+++ src/com/advanced/Jcase/GenericType/ClassDemo.java
@@ -0,0 +1,27 @@
+package com.advanced.Jcase.GenericType;
+
+/**
+ * 泛型类
+ */
+public class ClassDemo<T> {
+    private T t;
+
+    public void add(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public static void main(String[] args) {
+        ClassDemo<Integer> integerBox = new ClassDemo<Integer>();
+        ClassDemo<String> stringBox = new ClassDemo<String>();
+
+        integerBox.add(new Integer(10));
+        stringBox.add(new String("菜鸟教程"));
+
+        System.out.printf("整型值为 :%d\n\n", integerBox.get());
+        System.out.printf("字符串为 :%s\n", stringBox.get());
+    }
+}
