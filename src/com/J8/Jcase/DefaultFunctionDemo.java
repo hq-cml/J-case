50056a2f593831638f6f495b655f0799b9efcb4f
hq <hq_cml@163.com>
2018-04-07 17:13:38 +0800

    接口的默认函数

---
+++ src/com/J8/Jcase/DefaultFunctionDemo.java
@@ -0,0 +1,36 @@
+package com.J8.Jcase;
+
+/**
+ * 接口的默认函数
+ */
+public class DefaultFunctionDemo {
+    public static void main(String args[]){
+        Vehicle vehicle = new Car1();
+        vehicle.print();
+    }
+}
+
+interface Vehicle {
+    default void print(){
+        System.out.println("我是一辆车!");
+    }
+
+    static void blowHorn(){
+        System.out.println("按喇叭!!!");
+    }
+}
+
+interface FourWheeler {
+    default void print(){
+        System.out.println("我是一辆四轮车!");
+    }
+}
+
+class Car1 implements Vehicle, FourWheeler {
+    public void print(){
+        Vehicle.super.print();
+        FourWheeler.super.print();
+        Vehicle.blowHorn();
+        System.out.println("我是一辆汽车!");
+    }
+}
